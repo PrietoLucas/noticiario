874235e5423c241a2b83a446919fec6e633c3d8f
Lucas Prieto de Senna <lucas-prieto@hotmail.com>
2020-02-07 16:32:08 -0300

    correção do botão

--- js/script.js
+++ js/script.js
@@ -10,10 +10,10 @@ let config = {
 
 function mostrarNoticia(listaNoticias) {
     listaNoticias.forEach((noticia) => {
-        let noticiaNova = `<div class="col-md-4">
+        let noticiaNova = `<div class="col-md-4 mt-3 ">
         <div class="card" style="width: 18rem;">
-            <img src="${noticia.urlToImage}" class="card-img-top" alt="Imagem da Noticia">
             <div class="card-body text-justify">
+            <img src="${noticia.urlToImage}" class="card-img-top" alt="Imagem da Noticia">
               <h3 class="card-title text-center">${noticia.title}</h3>
               <p class="card-text">${noticia.description}</p>
               <a href="${noticia.url}" class="btn btn-primary">Ver Noticia</a>
